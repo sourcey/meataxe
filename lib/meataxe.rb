6ec16f5a60ebc07195ed8fb4d4b0672bf8c90f7f
Kam <auscaster@gmail.com>
2017-10-17 17:10:07 +0200

    Add support for multiple database adapters

--- lib/meataxe.rb
+++ lib/meataxe.rb
@@ -1,9 +1,1 @@
-# require_relative "meataxe/version"
-#
-# # Load custom tasks from `lib/capistrano/*` if you have any defined
-# # Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
-# # Dir.glob('lib/capistrano/**/*.rb').each { |r| import r }
-#
-# module Meataxe
-#   # :)
-# end
+# :)
